641bb26dbda964e591725b0f88224997e1915be8
Ravi Kiran Allipilli <luckyravi17@gmail.com>
2024-08-30 22:39:37 +0100

    minor tweaks in the query

--- tasks/task1.php
+++ tasks/task1.php
@@ -5,15 +5,11 @@ include __DIR__ . '/../views/header.html';
 
 echo "<div style=text-align:left;background-color:black;color:white;margin:30px;padding:20px;><p>
 -- Select email addresses from the 'emails' table <br>
--- Filter to include only profiles where 'Deceased' is 0 (i.e., not deceased) <br>
--- Group the results by 'UserRefID' and 'emailaddress' to aggregate data <br>
 -- The requirement is to filter the groups with more than one email address <br>
- -- And where the email is marked as 'Default' <br><br>
-SELECT e.emailaddress 
-FROM emails e 
-JOIN profiles p ON e.UserRefID = p.UserRefID 
-WHERE p.Deceased = 0 
-GROUP BY e.UserRefID, e.emailaddress; 
+ -- And where the email is marked as 'Default' <br><br> 
+SELECT e.emailaddress, e.UserRefID, e.Default, p.Deceased 
+FROM emails e JOIN profiles p ON e.UserRefID = p.UserRefID 
+WHERE p.Deceased = 0;
                                              
 </p></div>";
 
